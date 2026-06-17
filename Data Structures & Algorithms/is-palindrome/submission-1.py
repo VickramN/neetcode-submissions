797948f7cfe652d5f0ea39b91e101b72b5197cb4
FriendlyBear <110623631+VickramN@users.noreply.github.com>
2026-06-17 11:07:45 -0400

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.py
@@ -0,0 +1,5 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        sTemp = "".join(char for char in s if char.isalnum()).lower()
+        sTempRev = sTemp[::-1]
+        return sTempRev == sTemp
